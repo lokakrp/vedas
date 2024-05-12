6c10bdffd4065b263e267a811f738fd7efafeaaf
Christopher Bonner <cbonner.dev@outlook.com>
2024-05-13 00:31:46 +0100

    Js changes

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ async function generateHymns(path, number) {
   // Gets book data
   await fetchData(path).then((book) => {
     // For each hymn in book
+    let verseCount = 0;
     for (const hymnData in book) {
       // Clone hymn and get all relevant elements to do with that hymn
       let hymn = document.importNode(hymnTemplate.content, true);
@@ -25,11 +26,14 @@ async function generateHymns(path, number) {
 
       // For each verse in a hymn duplicate the paragraph elements
       for (let i = 0; i < book[hymnData].length; i++) {
+        verseCount++;
         let hymnVerse = hymn.querySelector(".hymn-verse").cloneNode(true);
         let firstPart = hymnVerse.querySelector(".first-line");
         let secondPart = hymnVerse.querySelector(".second-line");
         firstPart.textContent = book[hymnData][i][0];
         secondPart.textContent = book[hymnData][i][1];
+        hymnVerse.setAttribute("data-verse", verseCount);
+        hymnVerse.setAttribute("data-book", number);
         hymn.appendChild(hymnVerse);
       }
       // Remove template
@@ -44,4 +48,21 @@ async function generateHymns(path, number) {
 document.addEventListener("DOMContentLoaded", async function () {
   const book = document.title.split(" ")[document.title.split(" ").length - 1];
   await generateHymns(`books/filtered_book${book}.json`, book);
+
+  const verses = document.querySelectorAll(".hymn-verse");
+  verses.forEach((verse) => {
+    verse.addEventListener("click", () => {
+      let verseData = {
+        id: verse.getAttribute("data-verse"),
+        book: verse.getAttribute("data-book"),
+      };
+      let bookMarks =
+        JSON.parse(localStorage.getItem("bookmarks")) === null
+          ? []
+          : JSON.parse(localStorage.getItem("bookmarks"));
+      bookMarks.push(verseData);
+      localStorage.setItem("bookmarks", JSON.stringify(bookMarks));
+      console.log(localStorage.getItem("bookmarks"));
+    });
+  });
 });
